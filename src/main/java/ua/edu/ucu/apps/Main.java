d4b9721e165abcf9a0cf7d61e699677dc1b87aed
Viktoria Markus <148903307+Viktoria1122@users.noreply.github.com>
2024-12-08 19:47:41 +0200

    Update Main.java

--- src/main/java/ua/edu/ucu/apps/Main.java
+++ src/main/java/ua/edu/ucu/apps/Main.java
@@ -1,62 +1,30 @@
 package ua.edu.ucu.apps;
 
 import java.time.LocalDateTime;
-import ua.edu.ucu.apps.AuthMethod;
-
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Authenticate the user
-            AuthMethod authMethod = AuthMethod.FACEBOOK;
-            User user = authenticateUser(authMethod);
+        AuthMethod authMethod = AuthMethod.FACEBOOK;
 
-            if (user != null) {
-                System.out.println("Authenticated user: "
-                        + user.getUserMail()
-                        + ", Country: " + user.getCountry()
-                        + ", Last Active: " + user.getLastActiveTime());
-            } else {
-                System.err.println("Authentication failed for method: " + authMethod);
-            }
+        User user = authenticateUser(authMethod);
 
-            // Fetch company details
-            Company company = fetchCompanyDetails("Example Company");
-                        if (company != null) {
-                            System.out.println("Company Details: " + company);
-                        }
-                    } catch (Exception e) {
-                        System.err.println("Unexpected error: " + e.getMessage());
-                    }
-                }
-            
-                private static Company fetchCompanyDetails(String string) {
-                    // TODO Auto-generated method stub
-                    throw new UnsupportedOperationException("Unimplemented method 'fetchCompanyDetails'");
-                }
-            
-                /**
-     * Authenticate a user based on the given authentication method.
-     *
-     * @param authMethod Authentication method to use.
-     * @return Authenticated User or null if authentication fails.
-     */
-    private static User authenticateUser(AuthMethod authMethod) {
-        if (authMethod == null) {
-            System.err.println("No authentication method provided.");
-            return null;
+        if (user != null) {
+            System.out.println("Authenticated user: " 
+                + user.getUserMail() 
+                + ", Country: " + user.getCountry() 
+                + ", Last Active: " + user.getLastActiveTime());
         }
+    }
 
+    private static User authenticateUser(AuthMethod authMethod) {
         User user = null;
         switch (authMethod) {
             case FACEBOOK:
-                FacebookUser facebookUser = new FacebookUser(
-                        "user@example.com", "USA", LocalDateTime.now().toString());
+                FacebookUser facebookUser = new FacebookUser("user@example.com", "USA", LocalDateTime.now().toString());
                 user = new FacebookUserAdapter(facebookUser);
                 break;
             case TWITTER:
-                TwitterUser twitterUser = new TwitterUser(
-                        "user@twitter.com", "UK", LocalDateTime.now().minusDays(1).toString());
+                TwitterUser twitterUser = new TwitterUser("user@twitter.com", "UK", LocalDateTime.now().minusDays(1).toString());
                 user = new TwitterUserAdapter(twitterUser);
                 break;
             default:
